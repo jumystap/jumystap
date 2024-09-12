e28d531e5614d99d2468c8c0838afa5edd03dbf0
aidosgal <agalimzhan928@gmail.com>
2024-09-12 12:32:05 +0500

    fix: add certificate error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -76,8 +76,13 @@ class UserController extends Controller
             'role' => 'required',
             'date_of_birth' => 'nullable',
             'gender' => 'nullable',
+            'professions_ids' => 'nullable|array',
+            'professions_ids.*' => 'nullable|exists:professions,id',
+            'certificate_numbers' => 'nullable|array',
+            'certificate_numbers.*' => 'nullable|string',
             'description' => 'nullable|string',
             'source' => 'nullable|string',
+            'is_graduate' => 'nullable',
         ]);
 
         try {
@@ -117,7 +122,12 @@ class UserController extends Controller
             'ipStatus1' => 'nullable|string',
             'ipStatus2' => 'nullable|string',
             'ipStatus3' => 'nullable|string',
+            'professions_ids' => 'nullable|array',
+            'professions_ids.*' => 'nullable|exists:professions,id',
+            'certificate_numbers' => 'nullable|array',
+            'certificate_numbers.*' => 'nullable|string',
             'description' => 'nullable|string',
+            'is_graduate' => 'nullable',
             'age' => 'nullable'
         ]);
 
