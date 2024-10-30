f2fb55583cebce6994eee4b8a1c990d91fb9623e
Aidos Galimzhan <85453907+aidosgal@users.noreply.github.com>
2024-10-30 12:48:42 +0500

    Update GuestLayout.jsx

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -26,7 +26,7 @@ const items = [
     key: "1",
     label: (
       <Link href="/reviews" className="block py-2 flex items-center">
-        Отзывы
+        Оставить отзыв
       </Link>
     ),
   },
