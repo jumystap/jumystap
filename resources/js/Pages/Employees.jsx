cb0e3491def641cf474bdc16310243276201866b
Aidos Galimzhan <85453907+aidosgal@users.noreply.github.com>
2024-10-30 16:37:31 +0500

    Update Employees.jsx

--- resources/js/Pages/Employees.jsx
+++ resources/js/Pages/Employees.jsx
@@ -78,7 +78,7 @@ export default function Employees({ auth, employees, professions, filters = {} }
                     type="text"
                     value={searchTerm}
                     onChange={handleSearchChange}
-                    placeholder={t('search_placeholder', { ns: 'employees' })}
+                    placeholder="Поиск по имени"
                     className="w-full pl-10 pr-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
                 />
             </div>
@@ -224,7 +224,7 @@ export default function Employees({ auth, employees, professions, filters = {} }
 
                 {/* Desktop Filters Sidebar */}
                 <div className='col-span-2 hidden md:block'>
-                    <div className="sticky top-24 bg-white rounded-xl shadow-sm p-6 border border-gray-200">
+                    <div className="sticky top-5 bg-white rounded-xl shadow-sm p-6 border border-gray-200">
                         <FilterSection>
                             <FilterSelect
                                 name="jobType"
