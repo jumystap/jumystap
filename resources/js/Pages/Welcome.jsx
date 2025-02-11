3351798492f302db86a06fbfc53475ebf893bc92
Abylaikhan Sultanbek <ablsdu@gmail.com>
2025-02-11 09:58:01 +0500

    Update Welcome.jsx

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -165,7 +165,7 @@ export default function Welcome({
                                 : `Пройди платное обучение `}
                         </div>
                         <div className="font-light md:mt-3">
-                            по курсу <b style="font-weight: bold;">"Основы заработка на Youtube"</b>
+                            по курсу <b>"Основы заработка на Youtube"</b>
                         </div>
                         <div className="flex gap-x-5 mt-3 items-center">
                             <a href="https://forms.gle/Mb2h6yuWh93YqgEXA" target="_blank"
