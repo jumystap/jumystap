4a77451d185912b216359c7d899d44f154a33ef2
Abylaikhan <a.sultanbek@a-i.kz>
2025-10-31 14:39:49 +0500

    locales folder renamed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,7 +35,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="ttps://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 
         <!-- Icon -->
         <link rel="apple-touch-icon" sizes="180x180" href="https://jumystap.kz/icon.svg">
