a81646e38b89078fe96ccccb8942beeb8ad1b0a5
Abylaikhan <a.sunltanbek@a-i.kz>
2026-07-22 12:36:38 +0500

    chore: remove baker profession from application form

--- resources/js/Components/FeedbackModal.jsx
+++ resources/js/Components/FeedbackModal.jsx
@@ -20,7 +20,6 @@ export default function FeedbackModal({ isOpen, onClose, onSubmit }) {
         t('electric_gas_welder'),
         t('sales_manager'),
         t('cabinet_furniture_manufacturing_basics'),
-        t('baker'),
         t('electrician_2nd_degree'),
         t('sewing_machine_operator'),
     ];
